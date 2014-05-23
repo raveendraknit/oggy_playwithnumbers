fec0272974158c670eb2d9f4c4ee529f9e8ccb1b
LinhNguyen <linhvn09@gmail.com>
2014-05-23 08:09:53 -0300

    Initialize project

---
+++ PlaywithNumbers/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlaywithNumbers::Application.config.secret_token = '502bfa9cafab4e0d8de2fde9950e6f33afe3869a104796c4e670f86218a7484f9abef3946aa0e34cc6fdc1a564781588bb3b60531e3bdccbac28c6d5846dc67a'
